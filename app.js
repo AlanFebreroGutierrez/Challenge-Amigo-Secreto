bae3decef0dbe1c48bf2f550ebbaad554a063bce
Alan Febrero Gutierrez <alanfebrerogutierrez@gmail.com>
2025-08-07 17:34:25 -0300

    Agregar nombre al array, limpia el input, muestra lista de nombres en pantalla, limpia la lista previa.

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 let amigos = [];
 
 function agregarAmigo() {
-    //Captura el input del usuario.
-    let input = document.querySelector("amigo");
+//Captura el input del usuario.
+    let input = document.querySelector("#amigo");
     let nombre = input.value.trim();
 
-    //Valida la entrada para que efectivamente se ingrese un nombre y no un espacio en blanco.
+//Valida la entrada para que efectivamente se ingrese un nombre y no un espacio en blanco.
     if (nombre === ""){
         alert("Ingrese un nombre.");
         return;
-    }
 }
-    
+
+//Agrega el nombre al array
+amigos.push(nombre);
+
+//limpia el input
+input.value = "";
+
+//Muestra la lista de amigos en pantalla
+mostrarLista();
+}
+
+function mostrarLista(){
+    let listaNombres = document.getElementById("listaAmigos");
+//Limpia la lista previa
+    listaNombres.innerHTML = "";
+
+    amigos.forEach(function(nombre){
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        listaNombres.appendChild(li);
+    })
+}
